0b11765fec79bf4b060e462b207f90eb987c185e
Ran <1242295651@qq.com>
2024-11-01 16:55:03 +0800

    add L1G-3000

--- L1G/code/3000.py
+++ L1G/code/3000.py
@@ -1,30 +1,14 @@
 from openai import OpenAI
 import json
-from config import api_key
-
-def internlm_gen(prompt, client):
-    '''
-    LLM生成函数
-    Param prompt: prompt string
-    Param client: OpenAI client 
-    '''
-    response = client.chat.completions.create(
-        model="internlm2.5-latest",
-        messages=[
-            {"role": "user", "content": prompt},
-        ],
-        stream=False
-    )
-    return response.choices[0].message.content
-
-client = OpenAI(
-    base_url="https://internlm-chat.intern-ai.org.cn/puyu/api/v1/",
-    api_key=api_key)
+from config import api_key,internlm_gen,client
 
 query = "strawberry”中有几个字母“r”"
 prompt = f"""
 # 要求
 接下来你需要对下面的问题进行回答，请对问题一步一步进行思考，并且输出思维过程中的每一个步骤。
+最后的输出结果请用<answer></answer>标签包裹。
+# 技巧
+你可以对于问题中的每个单词都分别去鉴定，写出每个单词的情况，并在结果处累加
 # 问题是
 {query}
 """
